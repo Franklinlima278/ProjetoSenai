8d0fbf93141a57e4f2fd3cfc4d6149d03baf84f6
Franklin1709 <franklinsouzalima.com>
2026-04-18 20:18:24 -0300

    Esse git não é a polícia mas bate

---
+++ demoBecked/src/main/java/com/projetoSenai/demoBecked/DemoBeckedApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSenai.demoBecked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBeckedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBeckedApplication.class, args);
+	}
+
+}
---
+++ demoBecked/src/test/java/com/projetoSenai/demoBecked/DemoBeckedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSenai.demoBecked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBeckedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
