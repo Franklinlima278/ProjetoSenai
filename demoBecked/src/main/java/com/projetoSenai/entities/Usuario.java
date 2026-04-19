735213fcd16525dba3e63d37022e254376a8b48d
Franklin1709 <franklinsouzalima.com>
2026-04-19 08:01:02 -0300

    atualização na entidade Usuario

--- demoBecked/src/main/java/com/projetoSenai/entities/Usuario.java
+++ demoBecked/src/main/java/com/projetoSenai/entities/Usuario.java
@@ -9,11 +9,56 @@ import jakarta.persistence.Id;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String nome;
     private String senha;
     private String email;
 
+    public Usuario () {}
+    
+    public Usuario (Integer id, String nome, String senha, String email) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    
+    
 
 }
 
